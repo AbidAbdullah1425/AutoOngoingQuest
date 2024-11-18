f746f8dada7e5e4e945777d8bddc4199879b78be
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2024-11-18 13:07:56 +0600

    Update logger.py

--- libs/logger.py
+++ libs/logger.py
@@ -1,10 +1,9 @@
 import logging
 
-# Basic Logger setup
 LOGS = logging.getLogger("AutoAnimeBot")
 LOGS.setLevel(logging.INFO)
 
-# Console handler for logs
+# Console Handler for logs
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
 
@@ -12,9 +11,3 @@ formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 console_handler.setFormatter(formatter)
 
 LOGS.addHandler(console_handler)
-
-# For Telethon Logging
-class TelethonLogger(logging.Handler):
-    def emit(self, record):
-        log_entry = self.format(record)
-        LOGS.info(log_entry)
